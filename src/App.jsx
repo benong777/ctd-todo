d1b7f38314eb0212f43ef0e219243baf40196eaf
Ben <ong.ben@gmail.com>
2025-09-02 14:05:25 -0700

    Added data fetch from airtable

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,71 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `${import.meta.env.VITE_PAT}`;
+
+  useEffect(() => {
+    const fetchTodos = async() => {
+      setIsLoading(true);
+      const options = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      try {
+        const resp = await fetch(url, options);
+        //-- Error
+        if (!resp.ok) {
+          //-- Authentication Error
+          if (resp.status === 401) {
+            throw new Error('Not authorized. Please log in.');
+          }
+          const fetchData = await resp.json();
+          if (fetchData.error) {
+            throw new Error(fetchData.error);
+          }
+          //-- All other errors
+          throw new Error('Error occurred while fetching todo list.');
+        }
+
+        //-- Successful fetch
+        const { records } = await resp.json();
+        const fetchedRecords = records.map((record) => {
+          const todo = {
+            id: record.id,
+            ...record.fields,
+          }
+          if (!record.fields.isCompleted) {
+            todo.isCompleted = false;
+          }
+          return todo;
+        });
+
+        console.log(fetchedRecords[0].title);
+        setTodoList([...fetchedRecords]);
+      } catch(error) {
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      } 
+    }
+    fetchTodos();
+
+    //-- Needed?
+    return () => {
+      // cleanup function
+    };
+  }, []);
 
   function addTodo(title) {
     const newTodo = { title, id: Date.now(), isCompleted: false }
@@ -33,6 +94,10 @@ function App() {
     setTodoList(updatedTodos);
   }
 
+  function dimissErrorHandler() {
+    setErrorMessage('');
+  }
+
   return (
     <div>
       <h1>Todo List</h1>
@@ -41,7 +106,15 @@ function App() {
         todoList={todoList}
         onCompleteTodo={completeTodo}
         onUpdateTodo={updateTodo}
+        isLoading={isLoading}
       />
+      {errorMessage && (
+        <div>
+          <hr />
+          <p>{errorMessage}</p>
+          <button type='button' onClick={dimissErrorHandler}>Dismiss</button>
+        </div>
+      )}
     </div>
   )
 }
--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,15 +1,17 @@
 import TodoListItem from "./TodoListItem";
 
-function TodoList({ todoList, onCompleteTodo, onUpdateTodo }) {
+function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
   const filteredTodoList = todoList.filter((item) => item.isCompleted === false);
   return (
     <div>
-      { filteredTodoList.length === 0 ? (
+      {isLoading ? <p>Todo list is loading...</p> : (
+        filteredTodoList.length === 0 ? (
           <p>Add todo above to get started</p>
         ) : (
           <ul>
             {filteredTodoList.map((todo) => <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo} onUpdateTodo={onUpdateTodo}  />)}
           </ul>
+        )
       )}
     </div>
   )
